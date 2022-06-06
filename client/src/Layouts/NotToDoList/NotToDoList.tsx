733d242c27b71bf9661c4dd94b945d6a50013086
Alejandra Garcia <mgarcia@veevart.com>
2022-06-05 22:48:41 -0500

    not to do list data from db

--- client/src/Layouts/NotToDoList/NotToDoList.tsx
+++ client/src/Layouts/NotToDoList/NotToDoList.tsx
@@ -1,109 +1,58 @@
 import React from "react";
 import { Component } from "react";
 import { propTypes } from "react-bootstrap/esm/Image";
+import { json } from "stream/consumers";
 import Activity, { category } from "./Activity/Activity";
 import "./NotToDoList.css";
 
 const { url } = require("../../config");
 
 interface props {
-  loggedUser: string
+  loggedUser: string;
 }
 
 interface state {
+  data: any;
 }
 
 export default class NotToDoList extends Component<props, state> {
   loggedUser: string;
   constructor(props: props) {
     super(props);
-    this.loggedUser=this.props.loggedUser
+    this.loggedUser = this.props.loggedUser;
+    this.getDataFromServer = this.getDataFromServer.bind(this);
+    this.state = { data: [] };
   }
-  categories: Array<category> = [
-    {
-      title: "Cosas que te hacen perder el tiempo",
-      userInput: [
-        "Lorem ipsum dolor sit",
-        "elenitlaborum ea molestias consequuntur eos",
-        "Repellendus laboriosam animi",
-      ],
-    },
-    {
-      title: "Cosas que te estresan",
-      userInput: [
-        "Lorem ipsum dolor sit",
-        "elenitlaborum ea molestias consequuntur eos",
-        "Repellendus laboriosam animi",
-      ],
-    },
-    {
-      title: "Cosas que te sientes obligado a hacer",
-      userInput: [
-        "Lorem ipsum dolor sit",
-        "elenitlaborum ea molestias consequuntur eos",
-        "Repellendus laboriosam animi",
-        "Dignissimos ratione nulla possimus mollitia fugiat labore autemillo veritatis aliquid!",
-      ],
-    },
-    {
-      title: "Cosas que drenan tu energia",
-      userInput: [
-        "Lorem ipsum dolor sit",
-        "elenitlaborum ea molestias consequuntur eos",
-        "Repellendus laboriosam animi",
-      ],
-    },
-    {
-      title: "Cosas que no tienen que hacerse",
-      userInput: [
-        "Lorem ipsum dolor sit",
-        "elenitlaborum ea molestias consequuntur eos",
-        "Repellendus laboriosam animi",
-      ],
-    },
-    {
-      title: "Cosas que no puedes controlar",
-      userInput: [
-        "Lorem ipsum dolor sit",
-        "elenitlaborum ea molestias consequuntur eos",
-        "Repellendus laboriosam animi",
-        "Dignissimos ratione nulla possimus",
-      ],
-    },
-  ];
+  categories: Array<category> = [];
+  anything: any;
 
-  getData() {
+  async getDataFromServer(): Promise<any> {
     const request = new Request(`${url}/note/byuser/${this.loggedUser}`, {
       method: "GET",
-      mode: 'no-cors',
+      headers: {
+        Accept: "application/json",
+      },
       //body: '{"email": "srt6221@gmail.com"}',
     });
     fetch(request)
       .then((response) => {
-        console.log(request.url);
-        console.log(response.status);
-        console.log(response.body);
-        
-        
-        
-        if (response.status >= 200 && response.status<300) {
-          return response.json();
+        if (response.status >= 200 && response.status < 300) {
+          const res = response.json();
+          console.log(res);
+
+          return res;
         } else {
           throw new Error("Something went wrong on API server!");
         }
       })
-      .then((response) => {
-        console.debug(response);
-        // ...
-      })
       .catch((error) => {
         console.log("error");
-        
+
         console.error(error);
       });
   }
+
   render() {
-    this.getData();
     return (
       <>
         <div className="ntdl-container">
@@ -120,7 +69,7 @@ export default class NotToDoList extends Component<props, state> {
               illo veritatis aliquid!
             </p>
           </div>
-          <Activity categories={this.categories}></Activity>
+          <Activity loggedUser="srt6221@gmail.com"></Activity>
         </div>
       </>
     );
