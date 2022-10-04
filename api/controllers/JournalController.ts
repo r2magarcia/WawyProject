2290894bf5a21356bba7b16581614b86c242e8ed
r2magarcia <ma.garcia2r@gmail.com>
2022-10-04 02:28:49 -0500

    journal entry

--- api/controllers/JournalController.ts
+++ api/controllers/JournalController.ts
@@ -6,18 +6,32 @@ import UserService from "../services/UserService";
 export async function insertEntry(req: Request, res: Response) {
   console.log();
 
-  const usuario = UserService.getIdByEmail(req.body.email);
-  console.log(usuario);
+  const records: any = await JournalService.getEntries(req.body.email).then;
+  console.log(records);
+  
+  if(records) {
+    const resp : any= await JournalService.updateEntry(
+      req.body.notas,
+      req.body.user,
+      req.body.seguimientoDiario,
+      req.body.proyectoSemanal,
+      req.body.fecha
+    );
+    res.status(201).json(resp);
+  } else{
+    const resp : any= await JournalService.createEntry(
+      req.body.notas,
+      req.body.user,
+      req.body.seguimientoDiario,
+      req.body.proyectoSemanal,
+      req.body.fecha
+    );
+    res.status(201).json(resp);
+  }
+
   
   
-  const resp : any= await JournalService.insertEntry(
-    req.body.notas,
-    req.body.user,
-    req.body.seguimientoDiario,
-    req.body.proyectoSemanal,
-    req.body.fecha
-  );
-  res.status(201).json(resp);
+
 }
 
 export async function getEntries(req: Request, res: Response) {
@@ -28,4 +42,12 @@ export async function getEntries(req: Request, res: Response) {
   console.log(resp);
   
   res.status(200).json(resp[0]);
+}
+
+export async function getEntryForUser(email: string) {
+  console.log('getEntries');
+  
+  
+  const resp = await JournalService.getEntries(email);
+  return resp.length;
 }
