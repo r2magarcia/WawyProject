8ce5cbbf84191bc5faffaf5dbf690afbd48ea85a
santy6221 <srt6221@gmail.com>
2022-10-03 22:53:22 -0500

    cambio db

--- api/singleton.ts
+++ api/singleton.ts
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 export default class singleton{
 
     private static dbc: any = mysql.createConnection({
-        host: 'localhost',
+        host: '82.180.130.26',
         user: 'u676173880_wawyroot',
         password: 'Wawydbpassword2022'
     }); 
