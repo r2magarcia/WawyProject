071a0f293ed4549920bdcb11ef289d37ff9cdbd7
santy6221 <srt6221@gmail.com>
2022-10-03 21:02:30 -0500

    Dropdown actividades

--- api/dist/index.js
+++ api/dist/index.js
@@ -31,9 +31,7 @@ var UserHasListController = __importStar(require("./controllers/User_NotToDoList
 var DiaryController = __importStar(require("./controllers/DairyController"));
 var EstadoEController = __importStar(require("./controllers/EstadosEController"));
 var AnswerController = __importStar(require("./controllers/AnswerController"));
-var QuestionController = __importStar(require("./controllers/QuestionController"));
 var JournalController = __importStar(require("./controllers/JournalController"));
-var DiagnosticoController = __importStar(require("./controllers/DiagnosticoController"));
 var app = express_1.default();
 var port = require("./config").port;
 // TODO: Organizar las rutas para admin y requerir una key para acceder a los datos
@@ -77,12 +75,12 @@ app.route("/emotion").get(EstadoEController.getAllEstados);
 app.route("/answer/byUser/:id").get(AnswerController.filterAnswerByUser);
 app.route("/login/:credentials").get(UserController.logIn);
 app.route("/register").post(UserController.createUser);
-app.route("/question").get(QuestionController.getAllQuestions);
+// app.route("/question").get(QuestionController.getAllQuestions);
 app.route("/journal").post(JournalController.insertEntry);
-app.route("/diagnostico").post(DiagnosticoController.insertEntry).get(DiagnosticoController.getAllDiagnosis);
+// app.route("/diagnostico").post(DiagnosticoController.insertEntry).get(DiagnosticoController.getAllDiagnosis);
 app.route("/bullet-journal").post(JournalController.insertEntry);
 app.route("/bullet-journal/:email").get(JournalController.getEntries);
-app.route("/assignDiagnosis").get(DiagnosticoController.asignDiagnosis);
+// app.route("/assignDiagnosis").get(DiagnosticoController.asignDiagnosis);
 // app.route('/diary')
 // .get(DiaryController.getAllNotes)
 // .post(DiaryController.createNote);
