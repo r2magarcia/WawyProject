0bbdba222f8174de445450962601c775cf1668f9
r2magarcia <ma.garcia2r@gmail.com>
2022-10-04 18:47:03 -0500

    front

--- client/src/Layouts/User/Chat/Chat.tsx
+++ client/src/Layouts/User/Chat/Chat.tsx
@@ -7,15 +7,12 @@ import "./Chat.css";
 export default class Chat extends Component {
   render() {
     return (
-      <div className="chatbot-container-embed">
         <iframe
+        className="chatbot-container-embed"
           id="inlineFrameExample"
           title="Inline Frame Example"
-          width="300"
-          height="200"
           src="http://www.terapiachat.cl/chatbot/"
         ></iframe>
-      </div>
     );
   }
 }
